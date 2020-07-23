fb2c15432697ec6a85f6e89d8ccaf9b3be663e7c
Ramandeep <Ramandeep231099@gmail.com>
2020-07-22 20:48:33 -0700

    r
    r

---
+++ lab14a/first-code.js
@@ -0,0 +1,11 @@
+say_hell= function(){ //variable
+count = 54; //Defining a
+            // variable = value (Initizaing)
+count = count *2; //variable = new_value
+//alert(count); // access the variable
+lengh = 7.32; //float new_value
+hello = "hello";
+msg = hello;
+alert(msg + 'World' + count)
+
+}
