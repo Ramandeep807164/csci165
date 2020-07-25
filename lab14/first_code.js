9345da678b9768c0304dee16061e7ebd7f459830
Ramandeep <Ramandeep231099@gmail.com>
2020-07-24 19:56:13 -0700

    .

--- lab14/first_code.js
+++ lab14/first_code.js
@@ -1,10 +1,13 @@
 //Function of addition button
 add = function() {
 result = parseInt(jQuery('#op1').html()) + parseInt(jQuery('#op2').html());
-text = jQuery('#op1').html() + " + " jQuery('#op2').html() + " = "
+// jQuery('#operand_1').html('123213213213')
+// alert(result);
+text = jQuery('#op1').html() + " + " jQuery('#op2').html() + " = ";
 jQuery('#result').html(text + result);
 }
 setup = function() {
+  //alert("hi");
 jQuery('#add_button').click(add);
 }
 jQuery(document).ready(setup)
